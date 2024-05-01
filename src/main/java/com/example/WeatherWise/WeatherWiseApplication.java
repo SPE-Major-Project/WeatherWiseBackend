7b6d762fa7bef23201b6a3034da52950da0730fa
Rishabh Teli <rishabhteli14@gmail.com>
2024-05-01 19:03:58 +0530

    Update WeatherWiseApplication.java

--- src/main/java/com/example/WeatherWise/WeatherWiseApplication.java
+++ src/main/java/com/example/WeatherWise/WeatherWiseApplication.java
@@ -16,21 +16,19 @@ public class WeatherWiseApplication {
 	}
 
 	@Bean
-        public CorsFilter corsFilter() {
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-         	final CorsConfiguration config = new CorsConfiguration();
-         	config.setAllowCredentials(true);
-         	config.addAllowedOrigin("*"); 
-         	config.addAllowedHeader("*"); 
-         	config.addAllowedMethod("OPTIONS");
-         	config.addAllowedMethod("HEAD");
-         	config.addAllowedMethod("GET");
-         	config.addAllowedMethod("PUT");
-         	config.addAllowedMethod("POST");
-         	config.addAllowedMethod("DELETE");
-         	config.addAllowedMethod("PATCH");
-         	source.registerCorsConfiguration("/**", config);
-         	return new CorsFilter(source);
-     	}
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://weatherwisef-ssvc:3000/"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
 
 }
