5c1748801616df15e1d8c131233e02760a700c9d
rishabh0014 <rishabhteli14@gmail.com>
2024-05-20 13:12:43 +0530

    f

--- src/main/java/com/example/WeatherWise/WeatherWiseApplication.java
+++ src/main/java/com/example/WeatherWise/WeatherWiseApplication.java
@@ -15,22 +15,22 @@ public class WeatherWiseApplication {
 		SpringApplication.run(WeatherWiseApplication.class, args);
 	}
 
-	@Bean
-	public CorsFilter corsFilter() {
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		final CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("*"); // this allows all origin
-		config.addAllowedHeader("*"); // this allows all headers
-		config.addAllowedMethod("OPTIONS");
-		config.addAllowedMethod("HEAD");
-		config.addAllowedMethod("GET");
-		config.addAllowedMethod("PUT");
-		config.addAllowedMethod("POST");
-		config.addAllowedMethod("DELETE");
-		config.addAllowedMethod("PATCH");
-		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter(source);
-	}
+//	@Bean
+//	public CorsFilter corsFilter() {
+//		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		final CorsConfiguration config = new CorsConfiguration();
+//		config.setAllowCredentials(true);
+//		config.addAllowedOrigin("*"); // this allows all origin
+//		config.addAllowedHeader("*"); // this allows all headers
+//		config.addAllowedMethod("OPTIONS");
+//		config.addAllowedMethod("HEAD");
+//		config.addAllowedMethod("GET");
+//		config.addAllowedMethod("PUT");
+//		config.addAllowedMethod("POST");
+//		config.addAllowedMethod("DELETE");
+//		config.addAllowedMethod("PATCH");
+//		source.registerCorsConfiguration("/**", config);
+//		return new CorsFilter(source);
+//	}
 
 }
