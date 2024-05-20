0ac61a13ea50632bc0031a8e0f6c0a5cab929d77
rishabh0014 <rishabhteli14@gmail.com>
2024-05-20 23:50:24 +0530

    try-10

--- src/main/java/com/example/WeatherWise/controller/UserController.java
+++ src/main/java/com/example/WeatherWise/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 import java.util.Map;
 @RestController
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 @RequestMapping("/user")
 public class UserController {
 
